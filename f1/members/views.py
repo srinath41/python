925a8294ad73d61be93763126893b6b98d0748b9
srinath41 <devarapallisrinath8@gmail.com>
2024-05-04 11:45:04 +0530

    f1 removed from python

--- f1/members/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def members(request):
-    return HttpResponse("Hello world!")
-def mainone(request):
-    return HttpResponse('homepage')
